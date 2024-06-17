7363c6b667d26863f0685e5054aa69a25eda4354
Bliend <monishas7122001@gmail.com>
2024-06-17 14:14:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LoadingPage from './Components/LoadingPage';
+
+
+function App() {
+  return (
+    <div className="App">
+     <LoadingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/LoadingPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './LoadingPage.css';
+
+function LoadingPage() {
+    const firstLine = "The website";
+    const secondLine = "is set to launch imminently.";
+    return (
+        <div className='comingsoon-container'>
+            <div className='background-text'>
+                <span className='background-text-left'>end</span>
+                <span className='background-text-right'>bli</span>
+            </div>
+            <div className='transparent-layer'>
+                <div className='announcement'>
+                    <div className='vertical-line'></div>
+                    <div className='announcement-text'>
+                        <div className='line-1 highlight'>
+                            {firstLine.split('').map((char, index) => (
+                                <span
+                                    key={index}
+                                    className={`letter letter-${index + 1} ${char === ' ' ? 'space' : ''}`}
+                                >
+                                    {char}
+                                </span>
+                            ))}
+
+                        </div><br />
+                        <div className='line line-2'>
+                            <span>{secondLine}</span>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingPage
